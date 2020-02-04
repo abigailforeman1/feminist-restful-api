810125389dfbe6e9d192417cf47e0bd4499a63f1
abigailforeman <forea005@gmail.com>
2020-02-04 11:58:48 +0000

    feminist website updates

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000 // either use the port provided in env or 4000 (my local server port for developing)
-const dbURI = process.env.MONGODBD_URI || 'mongodb://localhost/restful-api' // * location of my db on local machine
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/restful-api' // * location of my db on local machine
 const secret = process.env.SECRET || 'women rule' // either use the stored secret or the one provided on the other side of the OR
 
 module.exports = { port, dbURI, secret }
